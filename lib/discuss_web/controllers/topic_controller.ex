584bdc9e7755020f783c857b7427c03bfacc64d5
João Guilherme <t.joaoguilherme1@gmail.com>
2022-04-17 12:18:58 -0300

    feat: add route redirection

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,11 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
@@ -13,7 +18,11 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> render(conn, "new.html", changeset: changeset)
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
+        render(conn, "index.html")
       {:error, changeset} -> render(conn, "new.html", changeset: changeset)
     end
 
--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,9 +17,9 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
-    get "/topics/new", TopicController, :new
+    get "/", TopicController, :index
     post "/topics", TopicController, :create
+    get "/topics/new", TopicController, :new
 
   end
 
